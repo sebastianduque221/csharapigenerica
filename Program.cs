9e1a6f6f9c4d53c9fdce1dd19826b0545771c852
Sebastian Duque <sebastian.duque221@tau.usbmed.edu.co>
2025-03-02 20:02:23 -0500

    Actualización de Program.cs: configuración de servicios y Swagger

--- Program.cs
+++ Program.cs
@@ -1,40 +1,74 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using csharpapi.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ControlConexion>();
+builder.Services.AddSingleton<TokenService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Api Genérica C#",
+        Version = "v1",
+        Description = "API de prueba con ASP.NET Core y Swagger",
+        Contact = new OpenApiContact
+        {
+            Name = "Soporte API",
+            Email = "soporte@miapi.com",
+            Url = new Uri("https://miapi.com/contacto")
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Genérica C#");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
+app.UseSession();
+app.UseAuthorization();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var configuration = builder.Configuration;
+string jwtKey = configuration["Jwt:Key"];
+Console.WriteLine($"Clave JWT: {jwtKey}");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+string connectionString = configuration.GetConnectionString("SqlServer");
+Console.WriteLine($"Conexión a BD: {connectionString}");
 
 app.Run();
 
